b7db953450040c6f8bf4180e59a3ab96a721662e
modupalliharshini <harshinimodupalli123@gmail.com>
2026-05-01 15:04:06 +0530

    homepage grid fix

--- src/components/TopPicks.jsx
+++ src/components/TopPicks.jsx
@@ -36,7 +36,7 @@ const TopPicks = () => {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-10">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-x-6 md:gap-y-10">
           {[...schools, ...schools].slice(0, 8).map((s, idx) => (
             <SchoolCard key={`${s.id}-${idx}`} s={s} />
           ))}
